fa0c0f555d04204896f4abb4af44d2de46d0c56a
linglp <linglp@umich.edu>
2024-05-01 15:45:12 -0400

    add functionality to add files in folder

--- APITests/folder_structure_benchmark.py
+++ APITests/folder_structure_benchmark.py
@@ -57,7 +57,7 @@ project_id, asset_view_id = create_multi_layer_test_folders(
 # project id: "syn58615911"
 calculate_walk_folder_time(project_id="syn58615911", repeat=10)
 
-# # case 2: a dataset folder has 5 layer, and each layer has 5 folders
+# case 2: a dataset folder has 5 layer, and each layer has 5 folders
 project_id, asset_view_id = create_multi_layer_test_folders(
     num_folder_per_layer=5,
     num_layer=5,
@@ -66,7 +66,7 @@ project_id, asset_view_id = create_multi_layer_test_folders(
 # project id: syn58617307
 calculate_walk_folder_time(project_id="syn58617307", repeat=5)
 
-# # case 3: a dataset folder has 10 layers, and each layer has 2 folders
+# case 3: a dataset folder has 10 layers, and each layer has 2 folders
 project_id, asset_view_id = create_multi_layer_test_folders(
     num_folder_per_layer=2,
     num_layer=10,
@@ -75,7 +75,7 @@ project_id, asset_view_id = create_multi_layer_test_folders(
 # project id: syn58621243
 calculate_walk_folder_time(project_id="syn58621243", repeat=10)
 
-# keep the number of folders constant
+# # keep the number of folders constant
 
 # case 1: one layer with 10 folders
 project_id, asset_view_id = create_multi_layer_test_folders_two(
@@ -85,7 +85,7 @@ project_id, asset_view_id = create_multi_layer_test_folders_two(
     total_entities_to_create=200,
 )
 calculate_walk_folder_time(project_id="syn58798762", repeat=10)
-# project id: syn58798762
+# # project id: syn58798762
 
 # case 2: two layer. First layer has 50 folders.
 project_id, asset_view_id = create_multi_layer_test_folders_two(
@@ -95,7 +95,7 @@ project_id, asset_view_id = create_multi_layer_test_folders_two(
     total_entities_to_create=200,
 )
 calculate_walk_folder_time(project_id="syn58799439", repeat=10)
-# project id: syn58799439
+# # project id: syn58799439
 
 # case 3:
 project_id, asset_view_id = create_multi_layer_test_folders_two(
@@ -106,3 +106,27 @@ project_id, asset_view_id = create_multi_layer_test_folders_two(
 )
 calculate_walk_folder_time(project_id="syn58799662", repeat=10)
 # project id: syn58799662
+
+
+# keep the number of folders constant and folder structure constant
+# case1: 10 files per layer starting from the second layer
+project_id, asset_view_id = create_multi_layer_test_folders_two(
+    first_layer_num=50,
+    max_layer=4,
+    project_name="API test project - folder structure 7",
+    total_entities_to_create=200,
+    num_files=10,
+    test_folder_path="test_files",
+)
+calculate_walk_folder_time(project_id="syn58887872", repeat=10)
+
+# case1: 20 files per layer starting from the second layer
+project_id, asset_view_id = create_multi_layer_test_folders_two(
+    first_layer_num=50,
+    max_layer=4,
+    project_name="API test project - folder structure 8",
+    total_entities_to_create=200,
+    num_files=20,
+    test_folder_path="test_files",
+)
+calculate_walk_folder_time(project_id="syn58890699", repeat=10)
