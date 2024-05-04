44e25da8006c333e1c5af47eb472665d7fa0cbed
linglp <linglp@umich.edu>
2024-05-04 00:24:32 -0400

    package functions into classes

--- APITests/folder_structure_benchmark.py
+++ APITests/folder_structure_benchmark.py
@@ -2,10 +2,12 @@ import time
 
 from synapseutils import walk
 
-from test_resources_utils import (
-    create_multi_layer_test_folders,
-    create_multi_layer_test_folders_two,
-)
+# from test_resources_utils import (
+#     create_multi_layer_test_folders,
+#     create_multi_layer_test_folders_two,
+# )
+
+# from test_resources_utils import CreateTestFolders
 from utils import StoreRuntime
 
 
@@ -20,11 +22,13 @@ def calculate_walk_folder_time(project_id: str, repeat: int) -> None:
     syn = srt.login_synapse()
     duration = []
     for i in range(repeat):
-        start_time = time.time()
+        # start_time = time.time()
+        start_time = time.perf_counter()
         walked_path = walk(syn, project_id, includeTypes=["folder", "file"])
         # actually run the function
-        _ = list((walked_path))
-        end_time = time.time()
+        walked_path = list((walked_path))
+        # end_time = time.time()
+        end_time = time.perf_counter()
 
         print(f"{i} time trying, and the duration is {end_time - start_time}")
         duration.append(end_time - start_time)
@@ -46,87 +50,88 @@ def get_change(current, previous):
         print("there is something wrong")
 
 
-# goal is to figure out how much does it take to walk through different projects with different folder structure
+# # goal is to figure out how much does it take to walk through different projects with different folder structure
 
 # case 1: a dataset folder has 2 layer, and each layer has 2 folders
-project_id, asset_view_id = create_multi_layer_test_folders(
-    num_folder_per_layer=3,
-    num_layer=3,
-    project_name="API test project -folder structure 1",
-)
-# project id: "syn58615911"
-calculate_walk_folder_time(project_id="syn58615911", repeat=10)
+# create_test_folders = CreateTestFolders(max_depth=3)
 
-# case 2: a dataset folder has 5 layer, and each layer has 5 folders
-project_id, asset_view_id = create_multi_layer_test_folders(
-    num_folder_per_layer=5,
-    num_layer=5,
-    project_name="API test project -folder structure 2",
-)
-# project id: syn58617307
-calculate_walk_folder_time(project_id="syn58617307", repeat=5)
+# project_id, _= create_test_folders.create_multi_layer_test_folders(
+#     num_folder_per_layer=3,
+#     project_name="API test project -folder structure 1",
+# )
+# # project id: "syn58918330"
+# calculate_walk_folder_time(project_id="syn58918330", repeat=10)
 
-# case 3: a dataset folder has 10 layers, and each layer has 2 folders
-project_id, asset_view_id = create_multi_layer_test_folders(
-    num_folder_per_layer=2,
-    num_layer=10,
-    project_name="API test project -folder structure 3",
-)
-# project id: syn58621243
-calculate_walk_folder_time(project_id="syn58621243", repeat=10)
+# # # case 2: a dataset folder has 5 layer, and each layer has 5 folders
+# create_test_folders = CreateTestFolders(max_depth=5)
+# project_id, _ = create_test_folders.create_multi_layer_test_folders(
+#     num_folder_per_layer=5,
+#     project_name="API test project -folder structure 2",
+# )
+# # # project id: syn58617307
+# calculate_walk_folder_time(project_id="syn58617307", repeat=5)
 
-# # keep the number of folders constant
+# # # case 3: a dataset folder has 10 layers, and each layer has 2 folders
+# create_test_folders = CreateTestFolders(max_depth=10)
+# project_id, _ = create_test_folders.create_multi_layer_test_folders(
+#     num_folder_per_layer=2,
+#     project_name="API test project -folder structure 3",
+# )
+# # # project id: syn58621243
+# calculate_walk_folder_time(project_id="syn58621243", repeat=10)
 
-# case 1: one layer with 10 folders
-project_id, asset_view_id = create_multi_layer_test_folders_two(
-    first_layer_num=100,
-    max_layer=2,
-    project_name="API test project - folder structure 4",
-    total_entities_to_create=200,
-)
-calculate_walk_folder_time(project_id="syn58798762", repeat=10)
-# # project id: syn58798762
+# # # # keep the number of folders constant
 
-# case 2: two layer. First layer has 50 folders.
-project_id, asset_view_id = create_multi_layer_test_folders_two(
-    first_layer_num=50,
-    max_layer=4,
-    project_name="API test project - folder structure 5",
-    total_entities_to_create=200,
-)
-calculate_walk_folder_time(project_id="syn58799439", repeat=10)
-# # project id: syn58799439
-
-# case 3:
-project_id, asset_view_id = create_multi_layer_test_folders_two(
-    first_layer_num=25,
-    max_layer=8,
-    project_name="API test project - folder structure 6",
-    total_entities_to_create=200,
-)
-calculate_walk_folder_time(project_id="syn58799662", repeat=10)
-# project id: syn58799662
+# # # case 1: one layer with 10 folders
+# create_test_folders = CreateTestFolders(max_depth=2)
+# project_id, _= create_test_folders.create_multi_layer_test_folders_two(
+#     first_layer_num=100,
+#     project_name="API test project - folder structure 4",
+#     total_entities_to_create=200,
+# )
+# # # project id: syn58798762
+# calculate_walk_folder_time(project_id="syn58798762", repeat=10)
 
 
-# keep the number of folders constant and folder structure constant
-# case1: 10 files per layer starting from the second layer
-project_id, asset_view_id = create_multi_layer_test_folders_two(
-    first_layer_num=50,
-    max_layer=4,
-    project_name="API test project - folder structure 7",
-    total_entities_to_create=200,
-    num_files=10,
-    test_folder_path="test_files",
-)
-calculate_walk_folder_time(project_id="syn58887872", repeat=10)
+# # # case 2: two layer. First layer has 50 folders.
 
-# case1: 20 files per layer starting from the second layer
-project_id, asset_view_id = create_multi_layer_test_folders_two(
-    first_layer_num=50,
-    max_layer=4,
-    project_name="API test project - folder structure 8",
-    total_entities_to_create=200,
-    num_files=20,
-    test_folder_path="test_files",
-)
-calculate_walk_folder_time(project_id="syn58890699", repeat=10)
+# create_test_folders = CreateTestFolders(max_depth=4)
+# project_id, _  = create_test_folders.create_multi_layer_test_folders_fixed_entities(first_layer_num=50, project_name="API test project - folder structure 5", total_folders_to_create=200)
+# calculate_walk_folder_time(project_id="syn58799439", repeat=10)
+# # # project id: syn58799439
+
+# # # case 3:
+# create_test_folders = CreateTestFolders(max_depth=8)
+# project_id, _ = create_test_folders.create_multi_layer_test_folders_fixed_entities(first_layer_num=25, project_name="API test project - folder structure 6", total_folders_to_create=200)
+# calculate_walk_folder_time(project_id="syn58799662", repeat=10)
+# # # project id: syn58799662
+
+
+# # # keep the number of folders constant and folder structure constant
+# # # case1: 10 files per layer starting from the second layer
+# create_test_folders = CreateTestFolders(max_depth=4, test_folder_path="test_files")
+# project_id, _ = create_test_folders.create_multi_layer_test_folders_fixed_entities(first_layer_num=50, project_name="API test project - folder structure 7", total_folders_to_create=200, num_files=10)
+# calculate_walk_folder_time(project_id="syn58887872", repeat=10)
+
+
+# # case2: 20 files per layer starting from the second layer
+# create_test_folders = CreateTestFolders(max_depth=4, test_folder_path="test_files")
+# project_id, _ = create_test_folders.create_multi_layer_test_folders_fixed_entities(
+#     first_layer_num=50,
+#     project_name="API test project - folder structure 8",
+#     total_folders_to_create=200,
+#     num_files=20,
+# )
+# calculate_walk_folder_time(project_id="syn58890699", repeat=10)
+
+# # case3: 40 files per layer starting from the second layer
+# create_test_folders = CreateTestFolders(max_depth=4, test_folder_path="test_files")
+# project_id, _ = create_test_folders.create_multi_layer_test_folders_fixed_entities(
+#     first_layer_num=50,
+#     project_name="API test project - folder structure 9",
+#     total_folders_to_create=200,
+#     num_files=40,
+# )
+# calculate_walk_folder_time(project_id="syn58943676", repeat=10)
+
+calculate_walk_folder_time(project_id="syn58617307", repeat=10)
